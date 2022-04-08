b697eb1d4686528aa0e440d2386a0dd735424103
uytgb159 <rnalscjf00@naver.com>
2022-04-08 18:10:06 +0900

    5. seperate sentence into words

--- word_freq.py
+++ word_freq.py
@@ -5,7 +5,18 @@ print(sys.argv[0], sys.argv[1], sys.argv[2]) # ex) python word_freq.py para1 par
 
 f = open(sys.argv[1], 'r')
 
+sentence=""
+words=[]
+
 lines=f.readlines() #read all lines and each line is list's element
-print(lines)
+for i in range(0,len(lines),1):
+    sentence+=lines[i]
+
+list=sentence.split()
+
+for i in range(0,len(list),1):
+    list[i]=list[i].lower().strip('~!@#$%^&*()_+`=-[]\{}|;<>?,./')
+
+print(list)
 
 f.close()
