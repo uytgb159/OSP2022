127e22c71fd52b9f30a6399b117bc5297f5d699f
uytgb159 <rnalscjf00@naver.com>
2022-04-08 20:22:44 +0900

    6. make a word-freq dictionary

--- word_freq.py
+++ word_freq.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+from audioop import reverse
 import sys
 
 print(sys.argv[0], sys.argv[1], sys.argv[2]) # ex) python word_freq.py para1 para2
@@ -9,14 +10,24 @@ sentence=""
 words=[]
 
 lines=f.readlines() #read all lines and each line is list's element
+f.close()
+
 for i in range(0,len(lines),1):
     sentence+=lines[i]
-
+    
 list=sentence.split()
 
+dict={}
+
 for i in range(0,len(list),1):
+    
     list[i]=list[i].lower().strip('~!@#$%^&*()_+`=-[]\{}|;<>?,./')
+    if dict.get(list[i])==None:
+        dict[list[i]]=1
+    else:
+        dict[list[i]]+=1
+    
 
-print(list)
-
-f.close()
+keys=dict.keys()
+for k in keys:
+    print(k, ":",dict[k])
