7472ed2adfb338fc509abed38de36afe849925f7
uytgb159 <rnalscjf00@naver.com>
2022-04-08 17:26:21 +0900

    4. read all lines and each line is list's element

--- word_freq.py
+++ word_freq.py
@@ -5,4 +5,7 @@ print(sys.argv[0], sys.argv[1], sys.argv[2]) # ex) python word_freq.py para1 par
 
 f = open(sys.argv[1], 'r')
 
+lines=f.readlines() #read all lines and each line is list's element
+print(lines)
+
 f.close()
